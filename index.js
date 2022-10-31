0c13387952a9a1bf870fbef18260ccb9d205efc1
Matthew Hall <15344417+mhall@users.noreply.github.com>
2022-10-31 17:42:48 -0400

    Support for averaging multiple physical sensors

--- index.js
+++ index.js
@@ -19,34 +19,48 @@ function getLux(lightLevel) {
 }
 
 var hueBridgeIp = null;
+var hueBridgeApiTime = 0;
 
 async function getBridgeIp(bridgeId, log) {
-    try {
-      let bridges = await axios.get('https://discovery.meethue.com/');
-      bridges.data.forEach(bridge => {
-        if (bridge.id == bridgeId) hueBridgeIp = bridge.internalipaddress;
-      });
-    }
-    catch (e) { log("Error obtaining bridge IP"); }
+  if ((Math.round(Date.now() / 1000 / 60) - hueBridgeApiTime) >= 15) {
+      try {
+        let bridges = await axios.get('https://discovery.meethue.com/');
+        bridges.data.forEach(bridge => {
+          if (bridge.id == bridgeId) hueBridgeIp = bridge.internalipaddress;
+        });
+	log("Hue bridge found with IP " + hueBridgeIp);
+      }
+      catch (e) { log("Error obtaining bridge IP"); }
+      hueBridgeApiTime = Math.round(Date.now() / 1000 / 60);
+  } else
+    log("Hue discovery API rate limit exceeded");
 }
 
-async function getLightLevel(bridgeId, apiKey, sensorId, log) {
+async function getLightLevel(bridgeId, apiKey, sensorIds, log) {
   if (!hueBridgeIp) {
     await getBridgeIp(bridgeId, log);
   }
   if (hueBridgeIp) {
-    const apiEndpoint = "http://" + hueBridgeIp + "/api/" + apiKey + "/sensors/" + sensorId;
-    try {
-      let sensorData = await axios.get(apiEndpoint);
-      let level = sensorData.data.state.lightlevel;
-      if (typeof level == 'number') return getLux(level);
-      else log("Invalid sensor light level");
+    if (!Array.isArray(sensorIds)) {
+      sensorIds = [sensorIds];
     }
-    catch (e) {
-      log("Failed to get sensor light level");
-      getBridgeIp(bridgeId, log);
+    var lightLevel = 0;
+    for (let sensorId of sensorIds) {
+      const apiEndpoint = "http://" + hueBridgeIp + "/api/" + apiKey + "/sensors/" + sensorId;
+      try {
+        let sensorData = await axios.get(apiEndpoint);
+        let level = sensorData.data.state.lightlevel;
+        if (typeof level == 'number') lightLevel = lightLevel + getLux(level);
+        else log("Invalid sensor light level");
+      }
+      catch (e) {
+        log("Failed to get sensor light level");
+        getBridgeIp(bridgeId, log);
+      }
     }
-  } else { log("Failed to find Hue bridge, please check configuration"); }
+    lightLevel = lightLevel / sensorIds.length;
+    return lightLevel;
+  } else { log("Failed to find Hue bridge, please check configuration or try again later"); }
   return null;
 }
 
@@ -62,10 +76,10 @@ function Accessory(log, config) {
 
   var hueBridgeId = config.bridgeId;
   var hueBridgeKey = config.bridgeKey;
-  var hueSensorId = config.sensorId;
+  var hueSensorIds = config.sensorIds;
 
-  if (!hueBridgeId || !hueBridgeKey || !hueSensorId) {
-    log("Invalid bridge or sensor definition, please check configuration");
+  if (!hueBridgeId || !hueBridgeKey || !hueSensorIds) {
+    log("Invalid Hue bridge or sensor definition, please check configuration");
     fltChar.updateValue(Characteristic.StatusFault.GENERAL_FAULT);
     return;
   }
@@ -78,7 +92,7 @@ function Accessory(log, config) {
   var readings = [];
 
   function addReading() {
-    getLightLevel(hueBridgeId, hueBridgeKey, hueSensorId, log).then((reading) => {
+    getLightLevel(hueBridgeId, hueBridgeKey, hueSensorIds, log).then((reading) => {
       if (reading) {
         if (readings.length == maxElems) readings.shift();
         readings.push(reading);
